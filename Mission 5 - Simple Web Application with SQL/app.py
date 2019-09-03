5fe7d7985981508b193cf7213317091d62d12162
Severus <severusal1207@gmail.com>
2019-09-03 11:01:48 +0800

    Update app.py

--- Mission 5 - Simple Web Application with SQL/app.py
+++ Mission 5 - Simple Web Application with SQL/app.py
@@ -1,7 +1,7 @@
 # app.py
 
 # This file is responsible to handle flask operations
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 import sqlite3
 from database_handler import execute_sql
 from models import Band, BandBooking, ConcertBooking, Venue
@@ -26,9 +26,36 @@ def index():
 
     return render_template("index.html", all_bookings=all_bookings)
 
-@app.route("/edit_concert_booking/<int:booking_id>")
+@app.route("/edit_concert_booking/<int:booking_id>", methods=["GET", "POST"])
 def edit_concert_booking(booking_id):
-    return str(booking_id)
+    if request.method =="GET":
+    #read daat from database
+        connection = sqlite3.connect("concert.db")
+        cursor = connection.execute("SELECT * FROM ConcertBooking WHERE BookingID={}".format(booking_id))
+        data = cursor.fetchone()
+        cursor.close()
+        connection.close()
+
+        print(data)
+        if data is None:
+            return "enter a valid booking ID!"
+            
+        else:
+            c = ConcertBooking(data[0], data[1], data[2])
+
+        return render_template("edit_booking.html", c=c)
+    else:
+        new_date = request.form["date"]
+        new_venue_name = request.form["venue_name"]
+        c = ConcertBooking(booking_id, new_date, new_venue_name)
+
+        connection = sqlite3.connect("concert.db")
+        connection.execute(c.update_record())
+        connection.commit()
+        connection.close()
+        return redirect(url_for("index"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
